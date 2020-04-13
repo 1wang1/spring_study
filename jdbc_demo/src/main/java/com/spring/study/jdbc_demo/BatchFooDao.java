07a7d0c0b581f0723dd360a07f6b018472ca53c5
wangzhenkun <wangzhenkun@ruijie.com.cn>
2020-04-13 21:38:32 +0800

    事务前中后的信息

--- jdbc_demo/src/main/java/com/spring/study/jdbc_demo/BatchFooDao.java
+++ jdbc_demo/src/main/java/com/spring/study/jdbc_demo/BatchFooDao.java
@@ -1,52 +1,52 @@
-package com.spring.study.jdbc_demo;
-
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.BatchPreparedStatementSetter;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
-import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
-import org.springframework.jdbc.core.namedparam.SqlParameterSourceUtils;
-import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
-import org.springframework.stereotype.Repository;
-
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
-
-@Slf4j
-@Repository
-public class BatchFooDao {
-
-    @Autowired
-    private JdbcTemplate jdbcTemplate;
-    @Autowired
-    private NamedParameterJdbcTemplate namedParameterJdbcTemplate;
-
-    public void batchInsert() {
-        jdbcTemplate.batchUpdate("INSERT INTO FOO (BAR) VALUES (?)",
-                                 new BatchPreparedStatementSetter() {
-                                     @Override
-                                     public void setValues(PreparedStatement preparedStatement,
-                                                           int i) throws SQLException {
-                                         preparedStatement.setString(1, "b-" + i);
-                                     }
-
-                                     @Override
-                                     public int getBatchSize() {
-                                         return 2;
-                                     }
-                                 });
-
-        List<Foo> list = new ArrayList<>();
-        list.add(Foo.builder().id(100L).bar("b-100").build());
-        list.add(Foo.builder().id(101L).bar("b-101").build());
-        namedParameterJdbcTemplate.batchUpdate("INSERT INTO FOO(ID,BAR) VALUES(:id,:bar)",
-                                               SqlParameterSourceUtils.createBatch(list));
-
-    }
-
-
-}
+//package com.spring.study.jdbc_demo;
+//
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.jdbc.core.BatchPreparedStatementSetter;
+//import org.springframework.jdbc.core.JdbcTemplate;
+//import org.springframework.jdbc.core.RowMapper;
+//import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+//import org.springframework.jdbc.core.namedparam.SqlParameterSourceUtils;
+//import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
+//import org.springframework.stereotype.Repository;
+//
+//import java.sql.PreparedStatement;
+//import java.sql.ResultSet;
+//import java.sql.SQLException;
+//import java.util.ArrayList;
+//import java.util.List;
+//
+//@Slf4j
+//@Repository
+//public class BatchFooDao {
+//
+//    @Autowired
+//    private JdbcTemplate jdbcTemplate;
+//    @Autowired
+//    private NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+//
+//    public void batchInsert() {
+//        jdbcTemplate.batchUpdate("INSERT INTO FOO (BAR) VALUES (?)",
+//                                 new BatchPreparedStatementSetter() {
+//                                     @Override
+//                                     public void setValues(PreparedStatement preparedStatement,
+//                                                           int i) throws SQLException {
+//                                         preparedStatement.setString(1, "b-" + i);
+//                                     }
+//
+//                                     @Override
+//                                     public int getBatchSize() {
+//                                         return 2;
+//                                     }
+//                                 });
+//
+//        List<Foo> list = new ArrayList<>();
+//        list.add(Foo.builder().id(100L).bar("b-100").build());
+//        list.add(Foo.builder().id(101L).bar("b-101").build());
+//        namedParameterJdbcTemplate.batchUpdate("INSERT INTO FOO(ID,BAR) VALUES(:id,:bar)",
+//                                               SqlParameterSourceUtils.createBatch(list));
+//
+//    }
+//
+//
+//}
--- jdbc_demo/src/main/java/com/spring/study/jdbc_demo/JdbcDemoApplication.java
+++ jdbc_demo/src/main/java/com/spring/study/jdbc_demo/JdbcDemoApplication.java
@@ -1,37 +1,55 @@
 package com.spring.study.jdbc_demo;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.TransactionCallbackWithoutResult;
+import org.springframework.transaction.support.TransactionTemplate;
 
 @SpringBootApplication
+@Slf4j
 public class JdbcDemoApplication implements CommandLineRunner {
 
     @Autowired
-    private FooDao fooDao;
-
+//    private FooDao fooDao;
+            TransactionTemplate transactionTemplate;
     @Autowired
-	private BatchFooDao batchFooDao;
+//	private BatchFooDao batchFooDao;
+            JdbcTemplate jdbcTemplate;
 
     public static void main(String[] args) {
         SpringApplication.run(JdbcDemoApplication.class, args);
     }
 
-    @Bean
-    @Autowired
-    public SimpleJdbcInsert simpleJdbcInsert(JdbcTemplate template) {
-        return new SimpleJdbcInsert(template).withTableName("FOO").usingGeneratedKeyColumns("ID");
-    }
+//    @Bean
+//    @Autowired
+//    public SimpleJdbcInsert simpleJdbcInsert(JdbcTemplate template) {
+//        return new SimpleJdbcInsert(template).withTableName("FOO").usingGeneratedKeyColumns("ID");
+//    }
 
     @Override
-	public void run(String...args) throws Exception{
+    public void run(String... args) throws Exception {
 //    	fooDao.insertData();
-    	batchFooDao.batchInsert();
-    	fooDao.listData();
+//    	batchFooDao.batchInsert();
+//    	fooDao.listData();
+        log.info("Count Before Transaction:{}", getCount());
+        transactionTemplate.execute(new TransactionCallbackWithoutResult() {
+            @Override
+            protected void doInTransactionWithoutResult(TransactionStatus transactionStatus) {
+                jdbcTemplate.execute("insert into FOO (ID,BAR) VALUES (2,'AAA')");
+                log.info("in Transaction:{}",getCount());
+                transactionStatus.setRollbackOnly();
+            }
+        });
+        log.info("Count after Transaction:{}",getCount());
+    }
 
-	}
+    private Long getCount() {
+        return (Long) jdbcTemplate.queryForList("select count(1) AS CNT from FOO").get(0)
+                .get("CNT");
+    }
 }
