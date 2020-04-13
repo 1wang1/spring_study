bb0dc7aef01bc4384d16ae6f823e5498502b6433
wangzhenkun <wangzhenkun@ruijie.com.cn>
2020-04-13 22:32:16 +0800

    添加类内部非事务方法调用事务方法没有对事务支持的解释

--- jdbc/jdbc_transaction/src/main/java/com/spring/study/jdbc_demo/JdbcDemoApplication.java
+++ jdbc/jdbc_transaction/src/main/java/com/spring/study/jdbc_demo/JdbcDemoApplication.java
@@ -16,6 +16,7 @@ import org.springframework.transaction.support.TransactionTemplate;
 @EnableTransactionManagement(mode = AdviceMode.PROXY)
 @Slf4j
 public class JdbcDemoApplication implements CommandLineRunner {
+
     @Autowired
     private FooService fooService;
     @Autowired
@@ -36,16 +37,25 @@ public class JdbcDemoApplication implements CommandLineRunner {
         } catch (Exception e) {
             log.info("BBB {}",
                      jdbcTemplate
-                             .queryForObject("SELECT COUNT(*) FROM FOO WHERE BAR='BBB'", Long.class));
+                             .queryForObject("SELECT COUNT(*) FROM FOO WHERE BAR='BBB'",
+                                             Long.class));
         }
 
         try {
-            // 因为该方法没有事务注解，所以不会以事务的方式进行调用；即使内部调用有事务的方法，也不会按照事务的方式执行
+            // spring 中为类做了一个代理
+            //需要调用代理类，才能执行到到被代理类的增强方法
+            //如果方法是在类的内部调用的话，意味着没有走到代理方法
+            //所以虽然invokeInsertThenRollback调用了一个带有transaction注解的方法
+            // 但是因为本身这个方法时没有事务的因此它在调用insertThenRollback的时候也不会有事务的支持
+//            spring先创建了自身的实例A，然后再加了一层封装的代理B，
+//            Spring Bean注入给其他人时，注入的不是A，而是B。所以通过Bean来调用时，调用的是B.xxx()，
+//            在A自身内部调用内部方法时，调用的还是A.xxx()。
             fooService.invokeInsertThenRollback();
         } catch (Exception e) {
             log.info("BBB {}",
                      jdbcTemplate
-                             .queryForObject("SELECT COUNT(*) FROM FOO WHERE BAR='BBB'", Long.class));
+                             .queryForObject("SELECT COUNT(*) FROM FOO WHERE BAR='BBB'",
+                                             Long.class));
         }
     }
 }
